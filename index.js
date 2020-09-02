3ef37ef0da33a94bba8fbbb7e394474bd685b72f
Muhammad Iqbal <Muhammad-Iqbal@users.noreply.github.com>
2020-09-02 12:55:48 +0500

    feature_update:: setup android example basic app and working other fe… (#1)
    * feature_update:: setup android example basic app and working other features
    
    * minor_update:: added Podfile.lock in gitignore + removed extra conditional check
    
    Co-authored-by: Muhammad Iqbal <iqbalm@theentertainerasia.com>

--- index.js
+++ index.js
@@ -3,7 +3,8 @@
  */
 
 import { AppRegistry } from "react-native";
-import Example from "./example/ios.example.index";
+import ExampleiOS from "./example/ios.example.index";
+import ExampleAndroid from "./example/ios.example.index";
 import { name as appName } from "./app.json";
 
-AppRegistry.registerComponent(appName, () => Example);
+AppRegistry.registerComponent(appName, () => ExampleAndroid);
